eae24833b5bc47a2e51d5b10b6b0a110aa1d6457
Yoga Bayu AP <yogabayusbi@gmail.com>
2024-04-21 21:43:29 +0700

    d

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -764,10 +764,12 @@
                                             </div>
                                         </div> --}}
                                         <!-- Modal -->
+                                        <!-- Modal -->
                                         <div class="modal fade" id="modalProject{{ $project->id }}" tabindex="-1"
                                             role="dialog" aria-labelledby="exampleModalCenterTitle"
                                             aria-hidden="true" data-backdrop="false">
-                                            <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
+                                            <div class="modal-dialog modal-dialog-centered modal-lg modal-dialog-scrollable"
+                                                role="document">
                                                 <div class="modal-content">
                                                     <div class="modal-header">
                                                         <h5 class="modal-title" id="exampleModalLongTitle">
