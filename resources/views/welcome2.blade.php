817ce4ab8902ac557dfe13b997a50a85a25d1192
Yoga Bayu AP <yogabayusbi@gmail.com>
2024-07-21 00:08:21 +0700

    update

--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -641,53 +641,6 @@
                 </header>
 
                 <section class="projects">
-                    {{-- <ul class="filter-list">
-                        <li class="filter-item">
-                            <button class="active" data-filter-btn>All</button>
-                        </li>
-
-                        <li class="filter-item">
-                            <button data-filter-btn>Web design</button>
-                        </li>
-
-                        <li class="filter-item">
-                            <button data-filter-btn>Applications</button>
-                        </li>
-
-                        <li class="filter-item">
-                            <button data-filter-btn>Web development</button>
-                        </li>
-                    </ul>
-
-                    <div class="filter-select-box">
-                        <button class="filter-select" data-select>
-                            <div class="select-value" data-selecct-value>
-                                Select category
-                            </div>
-
-                            <div class="select-icon">
-                                <ion-icon name="chevron-down"></ion-icon>
-                            </div>
-                        </button>
-
-                        <ul class="select-list">
-                            <li class="select-item">
-                                <button data-select-item>All</button>
-                            </li>
-
-                            <li class="select-item">
-                                <button data-select-item>Web design</button>
-                            </li>
-
-                            <li class="select-item">
-                                <button data-select-item>Applications</button>
-                            </li>
-
-                            <li class="select-item">
-                                <button data-select-item>Web development</button>
-                            </li>
-                        </ul>
-                    </div> --}}
 
                     <ul class="project-list">
                         @foreach ($projects as $project)
